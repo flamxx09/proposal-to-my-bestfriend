5326ab49c9d1a8ba6293413e28cf60af2b4a3380
flamxx09 <tandisuraj75@gmail.com>
2025-11-18 17:57:06 +0530

    2nd commit

--- src/data.js
+++ src/data.js
@@ -1,7 +1,7 @@
 // ==========================
 // 1. First Screen (Intro)
 // ==========================
-export const firstHeading = "suno madam ji humye apko kuch batana hai..."
+export const firstHeading = "suno popo ji humye apko kuch batana hai..."
 // Main heading text - Change this text as you like
 
 export const firstSubtext = "yesa kuch jisse sab kuch change ho jaega✨"
@@ -11,7 +11,7 @@ export const firstSubtext = "yesa kuch jisse sab kuch change ho jaega✨"
 // ==========================
 // 2. Question Screen
 // ==========================
-export const questionOne = "mujhe pta hai tujhe surprise bht pasand hai"
+export const questionOne = "mujhe pta hai apko surprise bht pasand hai"
 // First question text - Change this text as you like
 
 export const questionOneSubtext = "I promise it's worth it..."
@@ -20,14 +20,14 @@ export const questionOneSubtext = "I promise it's worth it..."
 export const questionTwo = "Do you like me?"
 // Second question text - Change this text as you like
 
-export const questionTwoSubtext = "Be honest dekh tujhe jo lgta whi bolna isme bura manne wali koi baat nahi hai..."
+export const questionTwoSubtext = "Be honest dekho apko jo lgta whi bolna isme bura manne wali koi baat nahi hai..."
 // Subtext under second question (optional) - Change, leave blank or remove.
 
 
 // ==========================
 // 3. Balloon Screen
 // ==========================
-export const balloonScreenHeading = "mai ye sab tujhe bht pehle se bolna chhata tha..."
+export const balloonScreenHeading = "mai ye sab apko bht pehle se bolna chhata tha..."
 // Heading text - Change this text as you like
 
 export const balloonScreenSubtext = "Watch the balloons carry my message to you"
@@ -40,10 +40,10 @@ export const balloonScreenEndText = "There's more I want to show you..."
 // ==========================
 // 4. Photo Screen
 // ==========================
-export const photoScreenHeading = "jab se tu mili hai tbse meri life m andhera cha gya hai pisachni,sorry mjk krra hu pr nhi tu jbse meri life mai aayi hai tbse life or bhi jyda brighter hogyi hai..."
+export const photoScreenHeading = "jab se ap mili ho tbse meri life m andhera cha gya hai pisachni,sorry mjk krra hu pr nhi aap jbse meri life mai aayi ho tbse life or bhi jyda brighter hogyi hai..."
 // Heading above photos - Change this text as you like
 
-export const photoScreenSubtext = "tune har movement special banaya hai💕"
+export const photoScreenSubtext = "apne har movement special banaya hai💕"
 // Subtext below heading (optional) - Change, leave blank or remove.
 
 // Add your photos inside "public/images" folder
@@ -62,7 +62,7 @@ export const photosScreenEndText = "There's more I want to show you..."
 // ==========================
 // 5. Final Screen (Proposal Message)
 // ==========================
-export const proposalMessage = ` Aach dekh mamta, mana mai pehla pyaar nahi hu tera, par  cahunga ki mai akhri bn skhu , jiski tu hakdaar thi , jitna tune kisi or chaha tha ,mai koshish krunga tujhe usse bhi jyda chach skhu or chunga tujhe mere hath har vo khushi mile or jb bi tu baithe mere sath, toh surf tujhe sukoon mile , or teri har baat mai bina btaye smjh jau , bas itna samjhna chhta hu tujhe or mujhe pta hai tu already ek pyare relationship hai jisse mai spoil nhi krna chhta hu pr ek baar puchna to bnta hai wrna regret rehh jayega jo ki m nahi chhta pr ek baat bta tu uske sath serious relationship mai hai na ? tu usse pyaar karti hai na? agr tu uske sath khus hai mujhe koi problm nhi hai q ki pyar ka shi mtlb paana nahi hota hai usse khus dekhna hota hai thoda filmy type hogya manta hu par tu agr uske sath khus hai na to thik hai koi problm nahi or yrr waise bhi mai rishte todne ka nhi jodne ke bare mai sochta hu , par ye mera last try hai dont worry...
+export const proposalMessage = ` aacha dekho popo mai janta hu aap bht busy rehte ho time nhi milta hai apko pr aap busy secudle se bhi time nikal ke mujhse baat krte n mujhe litrally bht aacha lgta hai jyda kuch to nhi pr itna bolna chhunga i love you end tak interval tak nahi or apna dhyn rakha kro smjheee
 
 I don’t know what the future holds, but I know one thing for sure.
 I want that future with you.`
